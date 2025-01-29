be014ab48f01d6887800613af1af8ff9cec18a80
billyh2 <billyh2@illinois.edu>
2025-01-29 11:37:24 -0600

    Initial Commit

---
+++ CursorTestTests/CursorTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  CursorTestTests.swift
+//  CursorTestTests
+//
+//  Created by Billy Huang on 2025/1/29.
+//
+
+import Testing
+
+struct CursorTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
